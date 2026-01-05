ba6acd8ebb2ccf995e5ee28e7c9a7c7e27bdadcd
Bruno Moraes <bruno_bmoraes@hotmail.com>
2026-01-05 16:42:53 -0300

    Configurando o TypeOrmModule com PostgreSQL (módulo do TypeORM para NestJS)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RecadosModule } from 'src/recados/recados.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [RecadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // ou 'bdtest' se estiver em docker-compose
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'bdtest',
+      autoLoadEntities: true,
+      synchronize: true, // ❌ não usar em produção
+    }),
+    RecadosModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
